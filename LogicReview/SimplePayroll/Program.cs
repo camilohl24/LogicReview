e41258de3ee0ce2a77012fd9ed602fd012518811
camilohl24 <juancamilohernandezhl@gmail.com>
2026-02-14 22:28:47 -0500

    Simple payroll

---
+++ LogicReview/SimplePayroll/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+using System;
+var answer = string.Empty;
+do
+{
+    Console.WriteLine("Payroll calculator");
+    var name = ConsoleExtension.GetString("Enter name.............: ");
+    var numberOfHours = ConsoleExtension.GetFloat("Enter hours worked.....: ");
+    var hourlyRate = ConsoleExtension.GetDecimal("Enter hourly rate......: ");
+    var minimumWage = ConsoleExtension.GetInt   ("Enter minimum wage.....: ");
+    var salary = (decimal)numberOfHours * hourlyRate;
+    if (salary < minimumWage)
+    {
+        Console.WriteLine($"Name of employee......: {name}");
+        Console.WriteLine($"Minimun wage..........: {minimumWage:C2}");
+    }
+    else
+    {
+        Console.WriteLine($"Name of employee........: {name}");
+        Console.WriteLine($"Salary Month............: {salary:C2}");
+    }
+
+    answer = ConsoleExtension.GetValidOptions("Deseas verificar otro empleado [S]i, [N]o : ", new List<string> { "S", "N" });
+
+}
+while (answer == "s");
+Console.WriteLine("Game over");
